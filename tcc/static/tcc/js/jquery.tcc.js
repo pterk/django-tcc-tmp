800872a10f85d8dda01ed51df6261b0c4f645baf
Peter van Kampen <pterk@datatailors.com>
2011-08-01 11:34:50 +0200

    highlight comment w/ hashtag

--- tcc/static/tcc/js/jquery.tcc.js
+++ tcc/static/tcc/js/jquery.tcc.js
@@ -30,13 +30,13 @@
     function debug(msg) {
         if(window.console && window.console.log){
             window.console.log(msg);
-        }
+        };
     };
 
     function listErrors(frm, errors){
         if($('ul.errors', frm).length == 0){ 
             $(frm).prepend('<ul class="errors"/>');
-        }
+        };
         $('ul.errors', frm).empty();
         $.each(errors, function(field, msgs){
             $.each(msgs, function(idx, msg){
@@ -44,7 +44,7 @@
                 $('ul.errors', frm).append(li);
             });
         });
-    }
+    };
 
     function isScrolledIntoView(elem){
       var docViewTop = $(window).scrollTop();
@@ -52,26 +52,34 @@
       var elemTop = $(elem).offset().top;
       var elemBottom = elemTop + $(elem).height();
       return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom));
-    }
+    };
 
     function apply_hooks(){
+
+        // highlight a thread
+        if(window.location.hash){
+            debug(window.location.hash);
+            $('a[name="' + window.location.hash.slice(1) +'"]').each(function(){
+                $(this).parent().addClass('highlight');
+            });
+        };
 
         if(opts.user_name){
             $('.c-user').filter(function(){
                 return $(this).text() == opts.user_name
             }).text(gettext('You'));
-        }
+        };
 
         function make_local_time(dte){
             var offset = -1 * new Date().getTimezoneOffset();
             return new Date(dte.valueOf()+(offset*JSMINUTE));
-        }
+        };
 
         function is_nowish(dte){
             // This is localtime (for the browser)
             var now = new Date();
             return (now-dte < JSMINUTE*5);
-        }
+        };
 
         function date_format(dte){
             var y=dte.getYear(), m=dte.getMonth(), d=dte.getDay(),
@@ -80,12 +88,12 @@
             if (m < 10) { m = '0' + m; }; 
             if (s < 10) { s = '0' + s; }; 
             return [y,m,d].join('-')+' '+[h, m].join(':');
-        }
+        };
 
         function days_ago(dte){
             var now = new Date();
             return parseInt((now-dte)/JSDAY);
-        }
+        };
 
         $('span.c-date').not('.humanized').each(function(){
             try {
@@ -104,7 +112,7 @@
                         humandate = gettext('just now');
                     } else {
                         humandate = gettext('today');
-                    }
+                    };
                     break;
                 case n==1:
                     humandate = gettext('yesterday');
@@ -117,12 +125,12 @@
                     break;
                 default:
                     humandate = date_format(dte);
-                }
+                };
                 $(this).text(humandate);
                 $(this).addClass('humanized');
             } catch (e) {
                 // pass
-            }
+            };
         });
 
         if(opts.user_id){
@@ -172,18 +180,18 @@
                 parent.append(frm);
                 if(!isScrolledIntoView(frm)){
                     $(document).scrollTop($(frm).offset().top-300);
-                }
+                };
                 $('#id_comment', frm).focus();
                 return false;
             });
-        }
-    }
+        };
+    };
 
     function init(){
         // showall is enable for everyone
         $('a.showall').click(function(){
             var parent = $(this).parent();
-            if($('ul.replies', parent).length == 0){ $(parent).append('<ul class="replies"/>');}
+            if($('ul.replies', parent).length == 0){ $(parent).append('<ul class="replies"/>');};
             var ul = $('ul.replies', parent).first();
             $.get($(this).attr('href'), function(data){
                 $(ul).html(data);
@@ -193,9 +201,9 @@
             return false;
         });
 
+        apply_hooks();
 
         if ( opts.user_id ) {
-            apply_hooks();
             // run this only once
             var frm = $('#tcc form').first();
             $(frm).submit(function(){
@@ -213,7 +221,7 @@
                 });
                 return false;
             });
-        }
-    }
+        };
+    };
 
 })(jQuery);
